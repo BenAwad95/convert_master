e90943db2fdd5dd7400b37dd9065647c3f8c8c5b
Abdullah Awad <abdullah.alashafi@gmail.com>
2022-02-22 20:56:05 +0300

    setup url for all project`s apps

--- control/views.py
+++ control/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
-
+PROJECT_APPS = {
+    'Jpg to Pdf': 'jpg2pdf',
+    'Pdf to Jpg': 'pdf2jpg',
+    'Word to Pdf': 'word2pdf',
+    'Pdf to Word': 'pdf2word'
+    }
 def home(request):
-    return render(request, 'base.html')
+    context = {
+        'apps': PROJECT_APPS
+    }
+    return render(request, 'base.html', context)
