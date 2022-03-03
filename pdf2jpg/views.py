9e4f5b8a497e97c7dcb660b08ac016a87a4aaedc
abdullah_awad <abdullah.alashafi@gmail.com>
2022-03-03 14:41:16 +0300

    form validate for pdf file field

--- pdf2jpg/views.py
+++ pdf2jpg/views.py
@@ -8,19 +8,22 @@ from .forms import Pdf2jpgForm
 def home(request):
     if request.method == 'POST':
         form = Pdf2jpgForm(request.POST, request.FILES)
-        pdf_file = request.FILES['pdf_file']
-        filename = pdf_file.name[:-3]
-        fs = FileSystemStorage()
-        pdf_file = fs.save(f'pdf2jpg-app/pdfs/{filename}.pdf', pdf_file)
-        pdf_file_path = fs.path(pdf_file)
-        # start covert
-        images = convert_from_path(pdf_file_path)
-        count = 1
-        urls = []
-        for image in images:
-            image.save(f'{settings.BASE_DIR}\\media\\pdf2jpg-app\\jpgs\\{filename}-{count}.jpg', 'JPEG')
-            urls.append(f'\\media\\pdf2jpg-app\\jpgs\\{filename}-{count}.jpg')
-            count+=1
-        return render(request, 'pdf2jpg\home.html', {'form': form, 'done': True, 'urls': urls})
+        if form.is_valid():
+            pdf_file = request.FILES['pdf_file']
+            filename = pdf_file.name[:-3]
+            fs = FileSystemStorage()
+            pdf_file = fs.save(f'pdf2jpg-app/pdfs/{filename}.pdf', pdf_file)
+            pdf_file_path = fs.path(pdf_file)
+            # start covert
+            images = convert_from_path(pdf_file_path)
+            count = 1
+            urls = []
+            for image in images:
+                image.save(f'{settings.BASE_DIR}\\media\\pdf2jpg-app\\jpgs\\{filename}-{count}.jpg', 'JPEG')
+                urls.append(f'\\media\\pdf2jpg-app\\jpgs\\{filename}-{count}.jpg')
+                count+=1
+            return render(request, 'pdf2jpg\home.html', {'form': form, 'done': True, 'urls': urls})
+        else:
+            return render(request, 'pdf2jpg\home.html', {'form': form})
     form = Pdf2jpgForm()
     return render(request, 'pdf2jpg\home.html', {'form': form})
