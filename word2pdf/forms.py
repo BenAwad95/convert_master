cba11dfb1548d563d2816ea839745f7916422324
abdullah_awad <abdullah.alashafi@gmail.com>
2022-03-03 14:47:42 +0300

    form validate for docx file field for word2pdf app

--- word2pdf/forms.py
+++ word2pdf/forms.py
@@ -1,6 +1,11 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django import forms
 
 
+def validate_jpg(value):
+    if not value.name.endswith('.docx'):
+        raise ValidationError(_('The file must docx!.'), params={'value': value})
 class Word2pdfForm(forms.Form):
     docx_file = forms.FileField(help_text='Select docs file.')
 
--- word2pdf/views.py
+++ word2pdf/views.py
@@ -8,15 +8,18 @@ from .forms import Word2pdfForm
 def home(request):
     if request.method == 'POST':
         form = Word2pdfForm(request.POST, request.FILES)
-        docx_file = request.FILES['docx_file']
-        filename = docx_file.name[:-4]
-        fs = FileSystemStorage()
-        docx_file = fs.save(f'word2pdf-app/docxs/{filename}.docx', docx_file)
-        docx_file_path = fs.path(docx_file)
-        # start convert
-        convert(docx_file_path, f'{settings.BASE_DIR}\\media\\word2pdf-app\\pdfs\\{filename}.pdf')
-        url = f'\\media\\word2pdf-app\\pdfs\\{filename}.pdf'
-        return render(request, 'word2pdf\home.html', {'form': form, 'done': True, 'url': url})
+        if form.is_valid():
+            docx_file = request.FILES['docx_file']
+            filename = docx_file.name[:-4]
+            fs = FileSystemStorage()
+            docx_file = fs.save(f'word2pdf-app/docxs/{filename}.docx', docx_file)
+            docx_file_path = fs.path(docx_file)
+            # start convert
+            convert(docx_file_path, f'{settings.BASE_DIR}\\media\\word2pdf-app\\pdfs\\{filename}.pdf')
+            url = f'\\media\\word2pdf-app\\pdfs\\{filename}.pdf'
+            return render(request, 'word2pdf\home.html', {'form': form, 'done': True, 'url': url})
+        else:
+            return render(request, 'word2pdf\home.html', {'form': form})
     form = Word2pdfForm()
     return render(request, 'word2pdf\home.html', {'form': form})
 
